06dc10ff4d4fc6dc6f370d0e82b396a9c3e3dc17
younesAmin <uhyounes@gmail.com>
2020-07-13 17:35:45 -0800

    fixing some spaces in the titles

--- info/info/spiders/bbc_spider.py
+++ info/info/spiders/bbc_spider.py
@@ -15,9 +15,14 @@ class BbcSpiderSpider(scrapy.Spider):
             url = li.css('.media__title a.media__link').xpath("@href").extract()
             category = li.css('.media__tag::text').extract()
             img_url = li.css('.media__image .responsive-image').css('::attr(src)').extract()
-
-            items['title'] = title
-            items['summary'] = summary
+            title1 = []
+            summary1 = []
+            for x in title:
+                title1.append(x.strip())
+            for x in summary:
+                summary1.append(x.strip())
+            items['title'] = title1
+            items['summary'] = summary1
             items['url'] = url
             items['category'] = category
             items['img_url'] = img_url
